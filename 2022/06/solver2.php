b4292e010a426189df4e113bdfa7df73432bea9a
Jon Jennings <jon@topdownview.com>
2022-12-06 02:24:25 -0800

    2022 day 06
    After yesterday's mess this felt a lot neater

---
+++ 2022/06/solver2.php
@@ -0,0 +1,14 @@
+<?php
+$signal = file( 'input.txt' );
+$signal = $signal[0];
+define( 'MARKER_LEN', 14 );
+
+for ( $ptr = 0; $ptr < strlen( $signal ) - MARKER_LEN; $ptr++ ) {
+	$sub = substr( $signal, $ptr, MARKER_LEN );
+	$set = array_unique( str_split( $sub ) );
+	if ( MARKER_LEN === count( $set ) ) {
+		break;
+	}
+}
+$ptr += MARKER_LEN;
+echo "Processed $ptr\n\r";
